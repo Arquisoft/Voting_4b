59ed49283e34f268dc7a7eb6ab34a8b7cceb6bba
daplerio <dario95tineo@gmail.com>
2016-04-28 18:15:51 +0200

    Añadidas nuevas clases al proyecto desde el original

--- VoteCounting_4b/src/test/java/es/uniovi/asw/WebTest.java
+++ VoteCounting_4b/src/test/java/es/uniovi/asw/WebTest.java
@@ -3,7 +3,7 @@ package es.uniovi.asw;
 import es.uniovi.asw.Calculate.voters.Referendum;
 import es.uniovi.asw.DBManagement.domain.Candidate;
 import es.uniovi.asw.DBManagement.domain.Vote;
-import es.uniovi.asw.WebController.Observer.WebObserver;
+
 import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 
